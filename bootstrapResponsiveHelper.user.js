f91fa564396061e410f2b805e602ba20b28b5fc7
Kevin BON <KevinBon@users.noreply.github.com>
2017-06-06 20:42:23 -0400

    Update bootstrapResponsiveHelper.user.js

--- bootstrapResponsiveHelper.user.js
+++ bootstrapResponsiveHelper.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Bootstrap responsive helper!
 // @author       Kevin BON (https://github.com/KevinBon)
-// @match        http://localhost/*
+// @match        https://getbootstrap.com/*
 // @grant        none
 // ==/UserScript==
 (function() {
@@ -25,10 +25,10 @@
 
   $(document.body).append('<div class="text-center" style="z-index: 9999; position: fixed; top: 0px;left: 50px;background-color: #444; color: wheat; font-weight: 100; box-shadow: 0px 2px 4px rgba(0,0,0,0.15);">' +
     '  <div style="display: inline-block; width: 50px;">' +
-    '    <div class="visible-xs-block bg-red"  style="padding: 1px 8px 1px 8px;">XS</div>' +
-    '    <div class="visible-sm-block bg-yellow" style="padding: 1px 8px 1px 8px;">SM</div>' +
-    '    <div class="visible-md-block bg-aqua" style="padding: 1px 8px 1px 8px;">MD</div>' +
-    '    <div class="visible-lg-block bg-blue" style="padding: 1px 8px 1px 8px;">LG</div>' +
+    '    <div class="visible-xs-block" style="padding: 1px 8px 1px 8px; background-color: #dd4b39;">XS</div>' +
+    '    <div class="visible-sm-block" style="padding: 1px 8px 1px 8px; background-color: #f39c12;">SM</div>' +
+    '    <div class="visible-md-block" style="padding: 1px 8px 1px 8px; background-color: #00c0ef;">MD</div>' +
+    '    <div class="visible-lg-block" style="padding: 1px 8px 1px 8px; background-color: #0073b7;">LG</div>' +
     '  </div>' +
     '  <div style="display: inline-block; padding: 1px 5px;" title="Width" id="debugResponsiveWidth">0</div>' +
     '  <div style="display: inline-block;">x</div>' +
